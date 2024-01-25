159e27ea630b58b5e7a792cefb5e742ac1817f83
JuanPabloDiaz <juanchodis@hotmail.com>
2024-01-25 17:54:41 -0500

    Update certifications section in Home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -11,9 +11,21 @@ const Home = () => {
       <div className="relative mb-4 flex items-center justify-center">
         <h1 className="text-md font-medium sm:text-xl">Certifications</h1>
       </div>
-      <div className="flex w-[500px] justify-center gap-4 text-center">
-        <img src={certificate1} alt="responsiveWebDesign" />
-        <img src={certificate2} alt="responsiveWebDesign" />
+      <div className="flex justify-center gap-4 text-center">
+        <a href="https://www.freecodecamp.org/certification/1diazdev/legacy-front-end">
+          <img
+            className="h-[400px]"
+            src={certificate1}
+            alt="Legacy Front End"
+          />
+        </a>
+        <a href="https://www.freecodecamp.org/certification/1diazdev/responsive-web-design">
+          <img
+            className="h-[400px]"
+            src={certificate2}
+            alt="Responsive Web Design"
+          />
+        </a>
       </div>
     </Layout>
   );
