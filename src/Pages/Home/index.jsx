220af8738f223b3c26e4a348f12430fb61a55bf7
JuanPabloDiaz <juanchodis@hotmail.com>
2024-01-25 18:03:07 -0500

    Update image sizes and add responsive layout

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -11,17 +11,17 @@ const Home = () => {
       <div className="relative mb-4 flex items-center justify-center">
         <h1 className="text-md font-medium sm:text-xl">Certifications</h1>
       </div>
-      <div className="flex justify-center gap-4 text-center">
+      <div className="flex flex-col justify-center gap-4 text-center lg:flex-row">
         <a href="https://www.freecodecamp.org/certification/1diazdev/legacy-front-end">
           <img
-            className="h-[400px]"
+            className="h-[200px] transition duration-300 ease-in-out hover:scale-95 hover:shadow-md hover:shadow-black md:h-[300px] lg:h-[400px]"
             src={certificate1}
             alt="Legacy Front End"
           />
         </a>
         <a href="https://www.freecodecamp.org/certification/1diazdev/responsive-web-design">
           <img
-            className="h-[400px]"
+            className="h-[200px] transition duration-300 ease-in-out hover:scale-95 hover:shadow-md hover:shadow-black md:h-[300px] lg:h-[400px]"
             src={certificate2}
             alt="Responsive Web Design"
           />
